e9aff0555552fbf3a500dccefa8ef80d221d9bf5
coffemocha <zhenhao.2001@gmail.com>
2025-02-20 15:55:08 +0800

    bug fix: Remove unnecessary |

--- src/main/java/godbot/task/Event.java
+++ src/main/java/godbot/task/Event.java
@@ -54,7 +54,7 @@ public class Event extends Task {
      */
     @Override
     public String toFileFormat() {
-        return "E | " + (isDone ? "1" : "0") + " | " + description + " | |" + from + " | " + to;
+        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + from + " | " + to;
     }
 }
 
